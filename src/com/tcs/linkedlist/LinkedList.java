0f96107ef3ef174595b4d2e22cc3eeb569dfcdf8
Utsav_Rastogi <st_utsavrastogi@live.com>
2020-04-25 01:10:44 +0530

    Changed the Single Linked List implementation.

--- src/com/tcs/linkedlist/LinkedList.java
+++ src/com/tcs/linkedlist/LinkedList.java
@@ -1,37 +1,145 @@
-package com.tcs.linkedlist;
-
-
+package com.tcs;
 
 public class LinkedList {
-	//This is the only variable of the ListNode class, stores the head of the list
+	//This is the only variable of ListNode class, used to store the head
 	private ListNode head;
 	
-	//This variable stores the length of the list
+	//Only variable of this class to store the length
 	private int length;
 	
-	//Returns the first node in the list
-	public synchronized ListNode getHead() {
-		return head;
+	//Returns the node at the head of the list
+	public ListNode getHead() {
+	return head;
 	}
 	
-	//Returns the length of the  list
 	public int length() {
-		return length;
+	return length;
 	}
 	
-	//The default constructor for this class
+	public synchronized void setHead(ListNode head) {
+		this.head = head;
+	}
+	
+	//Default constructor of this class
 	public LinkedList() {
-		length=0;
 		head=null;
+		length=0;
+	}
+
+	//Insert a node at the beginning of the list
+	public synchronized void insertAtBegin(Object data) {
+		ListNode newNode=new ListNode(data);
+		newNode.setNext(head);
+		head=newNode;
+		length++;
 	}
 	
-	//Removes all the elements and disposes the list
-	public void clearList() {
-		length=0;
-		head=null;
+	//Insert a node at the end of the list
+	public synchronized void insertAtEnd(Object data) {
+		ListNode newNode=new ListNode(data), p=null, q=null;
+		if(head==null) {
+			head=newNode;
+		}
+		else {
+			for(p=head;(q=p.getNext())!=null;p=q);
+			p.setNext(newNode);
+		}
+		length+=1;
+	}
+
+
+	//Insert a node at a given position
+	//All the nodes at that position till tail node move over to make the room
+	public synchronized void insert(Object data, int position) {
+		ListNode newNode =new ListNode(data), current=head;
+		//fix the position
+		if(position<0) {
+			position=0;
+		}
+		if(position>length) {
+			position=length;
+		}
+		if(head==null) {
+			head=newNode;
+		}
+		else if(position==0) {
+			newNode.setNext(head);
+			head=newNode;
+		}else {
+			for(int i=1;i<position;i++) {
+				current=current.getNext();
+			}
+			newNode.setNext(current.getNext());
+			current.setNext(newNode);
+		}
+		length++;
 	}
 	
-	//Returns the string representation of this collection, in the form ["str1", "str2"...]
+	//Remove a node at the head of the list
+	public synchronized ListNode removeFromBegin() {
+		ListNode temp=head;
+		if(head!=null) {
+			head=head.getNext();
+			temp.setNext(null);
+			length-=1;
+		}
+		return temp;
+		
+	}
+	
+	//Remove the tail node of the list
+	public synchronized ListNode removeFromEnd() {
+		ListNode current=head, previous=null;
+		if(head==null) {
+			return null;
+		}
+		else if(head.getNext()==null) {
+			head=null;
+			length-=1;
+			return current;
+		}else {
+			current=current.getNext();
+			while(current.getNext()!=null) {
+				previous=current;
+				current=current.getNext();
+			}
+			previous.setNext(null);
+			length-=1;
+			return current;
+		}
+	}
+	
+	//Remove a node at a specified position
+	public synchronized ListNode remove(int position) {
+		ListNode current=head;
+		//fix the position
+		if(position<0) {
+			position=0;
+		}
+		if(position>length) {
+			position=position-1;
+		}
+		
+		if(head==null) {
+			return null;
+		}
+		if(position==0) {
+			head=head.getNext();
+			length-=1;
+			return current;
+		}
+		else {
+			for(int i=1;i<position;i++) {
+				current=current.getNext();
+			}
+			ListNode temp=current.getNext();
+			current.setNext(current.getNext().getNext());
+			length-=1;
+			return temp;
+		}
+	}
+
+	//Returns the string representation of this collection in the form ["str1","str2"...]
 	@Override
 	public String toString() {
 		String result="[";
@@ -47,162 +155,62 @@ public class LinkedList {
 		return result+"]";
 	}
 	
-	//Insert a node at the beginning of the list
-	public synchronized void insertAtBegin(Object data) {
-		ListNode newNode=new ListNode(data);
-		newNode.setNext(head);
-		head=newNode;
-		length++;
-	}
-	
-	//Insert a new node at the end of the list
-	public synchronized void insertAtEnd(Object data) {
-		ListNode newNode =new ListNode(data), p, q;
-		if(head==null) {
-			head=newNode;
-		}else {
-			for(p=head;(q=p.getNext())!=null;p=q);
-			p.setNext(newNode);
-		}
-		length++;
-	}
-	
-	//Insert a node at a specified location
-	//All the nodes to that position till last move over to make the room
-	public synchronized void insert(Object data, int position) {
-		ListNode newNode=new ListNode(data), current=head;
-		//fix the position
-		if(position<0) {
-			position=0;
-		}
-		if(position>length) {
-			position=length;
-		}
-		//If the list is empty,make it be the only node
-		if(head==null) {
-			head=newNode;
-		}else
-		//If adding at the front of the list
-		if(position==0) {
-			newNode.setNext(head);
-			head=newNode;
-		}else
-			//find the location and insert the node
+	//Returns the last node in the list
+	public synchronized ListNode getLast() {
+		ListNode current=head;
+		if(head!=null)
 		{
-			for(int i=1;i<position;i++) {
+			while(current.getNext()!=null) {
 				current=current.getNext();
 			}
-			newNode.setNext(current.getNext());
-			current.setNext(newNode);
 		}
-		length++;
-		
-	}
-	
-	//Remove the node at the head of the list
-	public synchronized ListNode removeFromBegin() {
-		ListNode temp=head;
-		if(temp!=null) {
-			head=temp.getNext();
-			temp.setNext(null);
-			length-=1;
-		}
-		return temp;
-	}
-	
-	//Remove the node at the end of the list
-	public synchronized ListNode removeFromEnd() {
-		ListNode current=head, previous=null;
-		if(current==null) 
-			return null;
-		if(current.getNext()==null) {
-			head=null;
-			length-=1;
-			return head;
-		}
-		current=current.getNext();
-		while(current.getNext()!=null) {
-			previous=current;
-			current=current.getNext();
-		}
-		previous.setNext(null);
-		length-=1;
 		return current;
-			
-	}
-	
-	//Remove the node at a given position
-	public synchronized ListNode remove(int position) {
-		//fix the position
-		if(position<0) {
-			position=0;
-		}
-		if(position>length) {
-			position=length-1;
-		}
-		if(head==null)
-			return null;
-		if(position==0) {
-			ListNode current=head;
-			head=current.getNext();
-			length-=1;
-			return current;
-		}
-		ListNode current=head;
-		for(int i=1;i<position;i++) {
-			current=current.getNext();
-		}
-		ListNode removedNode=current.getNext();
-		current.setNext(current.getNext().getNext());
-		length-=1;
-		return removedNode;
 	}
 	
 	//Remove the matched node from the list
 	public synchronized ListNode removeMatched(Object data) {
-		ListNode current=head, previous;
+		ListNode current=head, previous=null;
 		if(head==null)
 			return null;
-		if(head.getData()==data){
-			head=current.getNext();
+		if(head.getData()==data) {
+			head=head.getNext();
 			length-=1;
 			return current;
-		}
-		while(current.getNext()!=null) {
-			previous=current;
-			current=current.getNext();
-			if(current.getData()==data) {
-				previous.setNext(current.getNext());
-				length-=1;
-				return current;
 			}
-			
+		else {
+			while(current.getNext()!=null) {
+				previous=current;
+				current=current.getNext();
+				if(current.getData()==data) {
+					previous.setNext(current.getNext());
+					length-=1;
+					return current;
+				}
+			}
 		}
 		return null;
-		
+			
 	}
 	
-
-	// Find the position of the first value that is equal to a given value.
-	// The equals method us used to determine equality.
-	public int getPosition(Object data) {
-		// go looking for the data
-		ListNode temp = head;
-		int pos = 0;
-		while (temp != null) {
-			if (temp.getData() == data) {
-				// return the position if found
-				return pos;
+	//Returns the first index position of an element
+	public synchronized int getPosition(Object data) {
+		ListNode current=head;
+		int index=0;
+		if(head!=null) {
+			while(current!=null) {
+				if(current.getData()==data) {
+					return index;
+				}
+				index+=1;
+				current=current.getNext();
 			}
-			pos += 1;
-			temp = temp.getNext();
 		}
-		// else return -1
 		return Integer.MIN_VALUE;
+}
+	//Clears the list
+	public void clearList() {
+		head=null;
+		length=0;
 	}
-	
-	// Size of the list.
-	public boolean isEmpty(){
-		return length==0;
-	}
+
 }
--- src/com/tcs/linkedlist/ListNode.java
+++ src/com/tcs/linkedlist/ListNode.java
@@ -1,62 +1,38 @@
-package com.tcs.linkedlist;
+package com.tcs;
 
 import java.util.Objects;
 
 public class ListNode {
-private Object data;
+	
+//Attributes of this class	
 private ListNode next;
-public Object getData() {
-	return data;
-}
-public void setData(Object data) {
-	this.data = data;
-}
+private Object data;
+
 public ListNode getNext() {
 	return next;
 }
 public void setNext(ListNode next) {
 	this.next = next;
 }
-
-public ListNode(Object data) {
-	this.data=data;
-	next=null;
+public Object getData() {
+	return data;
+}
+public void setData(Object data) {
+	this.data = data;
 }
 
-public ListNode() {
-	data=null;
-	next=null;
-}
-
-public static int listLength(ListNode headNode) {
-	ListNode current=headNode;
-	int length=0;
-	while(current!=null) {
-		length++;
-		current=current.getNext();
-	}
-	return length++;
-}
-
+//Overriding the toString method
 @Override
 public String toString() {
 	return data.toString();
 }
-@Override
-public int hashCode() {
-	return Objects.hash(data);
+
+//Only parameterized constructor of this class
+public ListNode(Object data) {
+	this.data = data;
 }
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	ListNode other = (ListNode) obj;
-	return Objects.equals(data, other.data);
-}
+
+
 
 
 }
